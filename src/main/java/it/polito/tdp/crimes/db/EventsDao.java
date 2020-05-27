ae1144c452b40fe7ddc6847e59fa1c1e4a97c5ad
OscarAlberto <s245739@studenti.polito.it>
2020-05-27 17:29:07 +0200

    Popolamento combobox

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -6,52 +6,104 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import it.polito.tdp.crimes.model.Event;
 
-
 public class EventsDao {
-	
-	public List<Event> listAllEvents(){
-		String sql = "SELECT * FROM events" ;
-		try {
-			Connection conn = DBConnect.getConnection() ;
 
-			PreparedStatement st = conn.prepareStatement(sql) ;
-			
-			List<Event> list = new ArrayList<>() ;
-			
-			ResultSet res = st.executeQuery() ;
-			
-			while(res.next()) {
+	public static List<Event> listAllEvents() {
+		String sql = "SELECT * FROM events";
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			List<Event> list = new ArrayList<>();
+
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
 				try {
-					list.add(new Event(res.getLong("incident_id"),
-							res.getInt("offense_code"),
-							res.getInt("offense_code_extension"), 
-							res.getString("offense_type_id"), 
-							res.getString("offense_category_id"),
-							res.getTimestamp("reported_date").toLocalDateTime(),
-							res.getString("incident_address"),
-							res.getDouble("geo_lon"),
-							res.getDouble("geo_lat"),
-							res.getInt("district_id"),
-							res.getInt("precinct_id"), 
-							res.getString("neighborhood_id"),
-							res.getInt("is_crime"),
-							res.getInt("is_traffic")));
+					list.add(new Event(res.getLong("incident_id"), res.getInt("offense_code"),
+							res.getInt("offense_code_extension"), res.getString("offense_type_id"),
+							res.getString("offense_category_id"), res.getTimestamp("reported_date").toLocalDateTime(),
+							res.getString("incident_address"), res.getDouble("geo_lon"), res.getDouble("geo_lat"),
+							res.getInt("district_id"), res.getInt("precinct_id"), res.getString("neighborhood_id"),
+							res.getInt("is_crime"), res.getInt("is_traffic")));
 				} catch (Throwable t) {
 					t.printStackTrace();
 					System.out.println(res.getInt("id"));
 				}
 			}
-			
+
 			conn.close();
-			return list ;
+			return list;
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null ;
+			return null;
 		}
+	}
+
+	public static List<String> listAllOffenseCategoryId() {
+
+		String sql = "SELECT DISTINCT offense_category_id " + "FROM events";
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			List<String> list = new ArrayList<>();
+
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				try {
+					list.add(res.getString("offense_category_id"));
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+
+			conn.close();
+			return list;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static List<Integer> listAllMonths() {
+
+		String sql = "SELECT DISTINCT MONTH(reported_date) " + "FROM EVENTS " + "ORDER BY MONTH(reported_date)";
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			List<Integer> list = new ArrayList<>();
+
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				try {
+					list.add(res.getInt("MONTH(reported_date)"));
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+
+			conn.close();
+			return list;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 }
--- src/main/java/it/polito/tdp/crimes/model/Model.java
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -1,6 +1,17 @@
 package it.polito.tdp.crimes.model;
 
+import java.util.List;
+
+import it.polito.tdp.crimes.db.EventsDao;
 
 public class Model {
-	
+
+	public List<String> listAllOffenseCategoryId() {
+		return EventsDao.listAllOffenseCategoryId();
+	}
+
+	public List<Integer> listAllMonths() {
+		return EventsDao.listAllMonths();
+	}
+
 }
